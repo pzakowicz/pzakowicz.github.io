87c47f6c26b790a16bf644dfa25df3ebd278421e
Piotr Z <piotr.zakowicz@gmail.com>
2020-06-04 09:17:05 +0200

    Updates

---
+++ precache-manifest.a135ad1c948060927dab5fac26d049ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0daf8a88f9a0ba3f715965d05fc7e19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59b073e3f268936350cf",
+    "url": "/static/css/main.8289e763.chunk.css"
+  },
+  {
+    "revision": "f44b3e18db9648463b52",
+    "url": "/static/js/2.711ab88e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.711ab88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b073e3f268936350cf",
+    "url": "/static/js/main.8f8e526f.chunk.js"
+  },
+  {
+    "revision": "b38f709691f005f4c585",
+    "url": "/static/js/runtime-main.e2af1984.js"
+  },
+  {
+    "revision": "dcb3395006b29d08d6e213a34082ca69",
+    "url": "/static/media/profile2.dcb33950.jpg"
+  }
+]);
